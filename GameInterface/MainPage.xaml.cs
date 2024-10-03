9d902c851384acab7f68e1059e25c62d6e34867a
Doaa Awan <106412925+Doaa-Awan@users.noreply.github.com>
2024-10-03 14:14:47 -0400

    Clean up code, add scoring and overlay
    - removed unnecessary comments
    - modified Move method in GamePiece class to take grid height and width
    - renamed variables
    - added text for score tracker and overlay before game start

--- GameInterface/MainPage.xaml.cs
+++ GameInterface/MainPage.xaml.cs
@@ -17,25 +17,19 @@ using GameLibrary;
 using Windows.UI.Xaml.Media.Imaging;
 
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 /* UWP Game Template
- * Created By: 
+ * Created By: Melissa VanderLely
  * Modified By:  
  */
 
-
 namespace GameInterface
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         private static GamePiece player;
         private static GamePiece dot;
 
-        private static List<GamePiece> collectedDots = new List<GamePiece>();  // List to store collected dots
+        private static List<GamePiece> collectedDots = new List<GamePiece>();  //List to store collected dots
         private Random random = new Random();
 
         // Declare the DispatcherTimer and direction
@@ -47,20 +41,24 @@ namespace GameInterface
             this.InitializeComponent();
             Window.Current.CoreWindow.KeyDown += CoreWindow_KeyDown;
 
-            player = CreatePiece("player", 60, 50, 50);                      //create a GamePiece object associated with the pac-man image
+            player = CreatePiece("player", 60, 50, 50);  //img, size, left, top
             dot = CreatePiece("dot", 15, 150, 150);
 
-            // Initialize direction and set up the timer
-            currentDirection = Windows.System.VirtualKey.Right; // Default direction
-            timer.Interval = TimeSpan.FromMilliseconds(50); // 100 ms for smooth movement
-            timer.Tick += Timer_Tick; // Attach Timer_Tick event to the timer
-            timer.Start(); // Start the timer
+            timer.Interval = TimeSpan.FromMilliseconds(50); 
+            timer.Tick += HandleMovement; 
+            timer.Start();
         }
 
-        private void Timer_Tick(object sender, object e)
+        private void CoreWindow_KeyDown(object sender, Windows.UI.Core.KeyEventArgs e)
+        {
+            currentDirection = e.VirtualKey;
+            lblOverlay.Visibility = Visibility.Collapsed;
+        }
+
+        private void HandleMovement(object sender, object e)
         {
             // Move the player continuously in the current direction
-            player.Move(currentDirection);
+            player.Move(currentDirection, gridMain.Height, gridMain.Width);
 
             // After moving, check for collisions
             if (IsCollected(player, dot))
@@ -70,23 +68,40 @@ namespace GameInterface
             }
         }
 
-        //private void CoreWindow_KeyDown(object sender, Windows.UI.Core.KeyEventArgs e)
-        //{
-        //    // Update the current direction based on the key press
-        //    currentDirection = e.VirtualKey;
-        //}
+        private void HandleDotCollection()
+        {
+            // Add the collected dot to the list
+            collectedDots.Add(dot);
 
-        /// <summary>
-        /// This method creates the Image object (to display the picture) and sets its properties.
-        /// It adds the image to the screen.
-        /// Then it calls the GamePiece constructor, passing the Image object as a parameter.
-        /// </summary>
-        /// <param name="imgSrc">Name of the image file</param>
-        /// <param name="size">Size in pixels (used for both dimensions, the images are square)</param>
-        /// <param name="left">Left location relative to parent</param>
-        /// <param name="top">Top location relative to parent</param>
-        /// <returns></returns>
-        private GamePiece CreatePiece(string imgSrc, int size, int left, int top)                 
+            //update score
+            txtScore.Text = (collectedDots.Count).ToString();
+
+            // Remove the dot from the screen
+            gridMain.Children.Remove(dot.PieceImg);
+
+            // Spawn a new dot at a random location
+            int newLeft = random.Next(0, (int)(gridMain.ActualWidth - 30)); // Prevents spawning off-screen
+            int newTop = random.Next(0, (int)(gridMain.ActualHeight - 30));
+            dot = CreatePiece("dot", 15, newLeft, newTop);
+        }
+
+        private bool IsCollected(GamePiece playerPiece, GamePiece dotPiece)
+        {
+            // Get the player's and dot's bounds
+            var playerBounds = playerPiece.PieceImg.TransformToVisual(gridMain)
+                .TransformBounds(new Windows.Foundation.Rect(0, 0, playerPiece.PieceImg.ActualWidth, playerPiece.PieceImg.ActualHeight));
+
+            var dotBounds = dotPiece.PieceImg.TransformToVisual(gridMain)
+                .TransformBounds(new Windows.Foundation.Rect(0, 0, dotPiece.PieceImg.ActualWidth, dotPiece.PieceImg.ActualHeight));
+
+            // Check if the player and the dot are intersecting
+            return (playerBounds.Left < dotBounds.Right &&
+                    playerBounds.Right > dotBounds.Left &&
+                    playerBounds.Top < dotBounds.Bottom &&
+                    playerBounds.Bottom > dotBounds.Top);
+        }
+
+        private GamePiece CreatePiece(string imgSrc, int size, int left, int top)
         {
             Image img = new Image();
             img.Source = new BitmapImage(new Uri($"ms-appx:///Assets/{imgSrc}.png"));
@@ -102,68 +117,6 @@ namespace GameInterface
             return new GamePiece(img);
         }
 
-        private void CoreWindow_KeyDown(object sender, Windows.UI.Core.KeyEventArgs e)
-        {
-            currentDirection = e.VirtualKey;
-
-            //Calculate new location for the player character
-            player.Move(e.VirtualKey);
-
-            //Check for collisions between player and collectible
-            //Note: this looks for identical top/left locations of the two objects. To be more precise, you can write a better collision detection method!
-
-            //if (player.Location == dot.Location)
-            //    await new MessageDialog("Collision Detected").ShowAsync();
-
-            // Check for collisions between player and dot
-            if (IsCollected(player, dot))
-            {
-                // Add the collected dot to the list
-                collectedDots.Add(dot);
-
-                // Remove the dot from the screen
-                gridMain.Children.Remove(dot.OnScreen);
-
-                // Notify the player
-                //await new MessageDialog("Dot Collected!").ShowAsync();
-
-                // Spawn a new dot at a random location
-                int newLeft = random.Next(0, (int)(gridMain.ActualWidth - 30)); // Prevents spawning off-screen
-                int newTop = random.Next(0, (int)(gridMain.ActualHeight - 30));
-                dot = CreatePiece("dot", 15, newLeft, newTop);
-            }
-        }
-
-        private void HandleDotCollection()
-        {
-            // Add the collected dot to the list
-            collectedDots.Add(dot);
-
-            // Remove the dot from the screen
-            gridMain.Children.Remove(dot.OnScreen);
-
-            // Spawn a new dot at a random location
-            int newLeft = random.Next(0, (int)(gridMain.ActualWidth - 30)); // Prevents spawning off-screen
-            int newTop = random.Next(0, (int)(gridMain.ActualHeight - 30));
-            dot = CreatePiece("dot", 15, newLeft, newTop);
-        }
-
-        // Improved collision detection method
-        private bool IsCollected(GamePiece playerPiece, GamePiece dotPiece)
-        {
-            // Get the player's and dot's bounds
-            var playerBounds = playerPiece.OnScreen.TransformToVisual(gridMain)
-                .TransformBounds(new Windows.Foundation.Rect(0, 0, playerPiece.OnScreen.ActualWidth, playerPiece.OnScreen.ActualHeight));
-
-            var dotBounds = dotPiece.OnScreen.TransformToVisual(gridMain)
-                .TransformBounds(new Windows.Foundation.Rect(0, 0, dotPiece.OnScreen.ActualWidth, dotPiece.OnScreen.ActualHeight));
-
-            // Check if the player and the dot are intersecting
-            return (playerBounds.Left < dotBounds.Right &&
-                    playerBounds.Right > dotBounds.Left &&
-                    playerBounds.Top < dotBounds.Bottom &&
-                    playerBounds.Bottom > dotBounds.Top);
-        }
 
     }
 }
--- GameLibrary/GamePiece.cs
+++ GameLibrary/GamePiece.cs
@@ -5,7 +5,7 @@ using Windows.UI.Xaml.Media;
 using Windows.Foundation;
 
 /* UWP Game Library
- * Written By: 
+ * Written By: Melissa VanderLely
  * Modified By:
  */
 
@@ -13,65 +13,67 @@ namespace GameLibrary
 {
     public class GamePiece
     {
-        private Thickness objectMargins;            //represents the location of the piece on the game board
-        private Image onScreen;                     //the image that is displayed on screen
-        private RotateTransform rotateTransform; // Transform to rotate the image
+        private Thickness position;            
+        private Image pieceImg;                   
+        private RotateTransform rotateTransform;
 
-        public Thickness Location                     //get access only - can not directly modify the location of the piece
+        public Thickness Location             
         {
-            get { return onScreen.Margin; }
+            get { return pieceImg.Margin; }
         }
 
-        public Image OnScreen => onScreen; //public property to access image
+        public Image PieceImg => pieceImg; //public property to access image
 
-        public GamePiece(Image img)                 //constructor creates a piece and a reference to its associated image
-        {                                           //use this to set up other GamePiece properties
-            onScreen = img;
-            objectMargins = img.Margin;
+        public GamePiece(Image img)  //creates a piece and a reference to its associated image
+        {
+            pieceImg = img;
+            position = img.Margin;
 
             // Initialize RotateTransform and set it as the image's render transform
             rotateTransform = new RotateTransform();
-            onScreen.RenderTransform = rotateTransform;
-            onScreen.RenderTransformOrigin = new Point(0.5, 0.5);  // Rotate around the center of the image
+            pieceImg.RenderTransform = rotateTransform;
+            pieceImg.RenderTransformOrigin = new Point(0.5, 0.5);  // Rotate around the center of the image
         }
 
-        public bool Move(Windows.System.VirtualKey direction)   //calculate a new location for the piece, based on a key press
+        //calculate a new location for the piece, based on a key press
+        public bool Move(Windows.System.VirtualKey direction, double gridHeight, double gridWidth)   
         {
+
             // Get the current margin (location) of the player
-            Thickness newMargins = objectMargins;
+            Thickness newMargins = position;
 
             switch (direction)
             {
                 case Windows.System.VirtualKey.Up:
-                    newMargins.Top = Math.Max(0, objectMargins.Top - 20);  // Ensure the top margin doesn't go below 0
-                    rotateTransform.Angle = 270;  // Rotate upwards (270 degrees)
-                    //objectMargins.Top -= 10;
+                    newMargins.Top = Math.Max(0, position.Top - 20);
+                    rotateTransform.Angle = 270;
                     break;
                 case Windows.System.VirtualKey.Down:
-                    newMargins.Top = Math.Min(700 - onScreen.Height, objectMargins.Top + 20);  // Prevent the player from moving below the grid
-                    rotateTransform.Angle = 90;  // Rotate downwards (90 degrees)
-                    //objectMargins.Top += 10;
+                    newMargins.Top = Math.Min(gridHeight - pieceImg.Height, position.Top + 20);
+                    rotateTransform.Angle = 90;
                     break;
                 case Windows.System.VirtualKey.Left:
-                    newMargins.Left = Math.Max(0, objectMargins.Left - 20);  // Ensure the left margin doesn't go below 0
-                    rotateTransform.Angle = 180;  // Rotate left (180 degrees)
-                    //objectMargins.Left -= 10;
+                    newMargins.Left = Math.Max(0, position.Left - 20);
+                    rotateTransform.Angle = 180;
                     break;
                 case Windows.System.VirtualKey.Right:
-                    newMargins.Left = Math.Min(700 - onScreen.Width, objectMargins.Left + 20);  // Prevent the player from moving beyond the grid
-                    rotateTransform.Angle = 0;  // Rotate right (0 degrees)
-                    //objectMargins.Left += 10;
+                    newMargins.Left = Math.Min(gridWidth - pieceImg.Width, position.Left + 20);
+                    rotateTransform.Angle = 0;
                     break;
                 default:
                     return false;
             }
+
+            // Recheck bounds to ensure the image is fully visible
+            newMargins.Left = Math.Max(0, Math.Min(gridWidth - pieceImg.ActualWidth, newMargins.Left));
+            newMargins.Top = Math.Max(0, Math.Min(gridHeight - pieceImg.ActualHeight, newMargins.Top));
+
             // Update only if the new location is within boundaries
-            objectMargins = newMargins;
+            position = newMargins;
 
-            onScreen.Margin = objectMargins;            //assign the new position to the on-screen image
+            pieceImg.Margin = position;   // Assign the new position to the on-screen image
             return true;
+
         }
-
-
     }
 }
